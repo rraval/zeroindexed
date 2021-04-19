a53918d4188032d9f1dc80caacd89c8df847e6dd
Ronuk Raval <ronuk.raval@gmail.com>
2021-04-19 00:02:09 -0400

    Manage blog DNS in Pulumi

--- packages/pulumi/src/index.ts
+++ packages/pulumi/src/index.ts
@@ -175,3 +175,19 @@ const _valheimCtl = new ValheimCtl(
     },
     {provider: clusterProvider},
 );
+
+// GitHub Pages DNS servers
+// https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site#configuring-an-apex-domain
+[
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+].forEach((ip, index) => {
+    new cloudflare.Record(`blog-${index}`, {
+        zoneId: cloudflareZoneId,
+        name: "@",
+        value: ip,
+        type: "A",
+    });
+});
