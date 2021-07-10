ce89a95115ae0f7b01d6d9aa64137488b42e03d6
Ronuk Raval <ronuk.raval@gmail.com>
2021-07-10 12:15:55 -0400

    Disable Valheim
    The whole point of `@zeroindexed/valheim` and `@zeroindexed/valheimctl`
    was to provide the cheapest possible way to run a Valheim server, with a
    web UI to start the server on demand and automated shutdown when idle.
    
    All the pieces worked great except for the `LoadBalancer` resource,
    which is always online even though the service it routes traffic to has
    no active pods.
    
    This ends up costing ~$18/month, which breaks the "cheapest possible"
    promise. Doing away with the `LoadBalancer` is going to require
    significant rethinking of how this is deployed.

--- packages/pulumi/src/index.ts
+++ packages/pulumi/src/index.ts
@@ -4,11 +4,9 @@ import {makeBlog} from "./blog";
 import {Config} from "./config";
 import {makeKubernetes} from "./kubernetes";
 import {makeToph} from "./toph";
-import {makeValheim} from "./valheim";
 
 const config = Config.fromPulumi();
 const kubernetes = makeKubernetes(config);
-const {cluster, provider} = kubernetes;
 
 // Not really a secret but it also prevents this big string from being printed
 // on every `pulumi up`.
@@ -16,9 +14,3 @@ export const kubeconfig = pulumi.secret(kubernetes.kubeconfig);
 
 makeToph(config);
 makeBlog(config);
-
-makeValheim({
-    config,
-    cluster,
-    provider,
-});
